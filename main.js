1117ceca7cbce3a6b85a2ff3461d1c5231f5a2cf
Ragina1301 <raginahaitbaeva@gmail.com>
2023-01-30 15:53:28 +0500

    my first project

---
+++ main.js
@@ -0,0 +1,100 @@
+// function plus(number) {
+//     let total = 0
+//     for(let i = 0; i <= number; i++){
+//         total += i
+//     }
+//     return total
+// }
+// console.log(plus(5))
+
+
+
+// function mango(quantity, price) {
+//     if(quantity<=5){
+//         console.log((quantity-1)*price);
+//     }
+//     else if(quantity<=6){
+//         console.log((quantity-2)*price);
+//     }
+//     else if(quantity<=9){
+//         console.log((quantity-3)*price);
+//     }
+// }
+// mango(9,3)
+
+
+
+// function mango(dona, narx) {
+//     return narx * (dona - Number.parseInt(dona / 3))
+// }
+// console.log(mango(5,3));
+
+
+
+// function ragina(kun){
+//     switch(kun){
+//         case 1 : console.log("1-yanvar"); break;
+//         case 2 : console.log("2-fevral"); break;
+//         case 3 : console.log("3-mart"); break;
+//         case 4 : console.log("4-aprel"); break;
+//         case 5 : console.log("5-may"); break;
+//         case 6 : console.log("6-iyun"); break;
+//         case 7 : console.log("7-iyul"); break;
+//         case 8 : console.log("8-avgust"); break;
+//         case 9 : console.log("9-sentyabr"); break;
+//         case 10 : console.log("10-oktyabr"); break;
+//         case 11 : console.log("11-noyabr"); break;
+//         case 12 : console.log("12-dekabr"); break;
+//         default : console.log("bunday oy yo'q");
+//     }
+// }
+// ragina(1)
+
+
+
+// let disemvowel = (str) => str.replace([a,e,i,o,u])
+// console.log(disemvowel(ragina));
+
+
+
+// function ism(ism) {
+//   console.log(
+//     ism
+//       .replace(/!/g, "")
+//       .replace(/@/g, "")
+//       .replace(/#/g, "")
+//       .replace(/%/g, "")
+//       .replace(/&/g, "")
+//       .replace(/o'/g, "")
+//       .replace(/A/g, "")
+//       .replace(/I/g, "")
+//       .replace(/U/g, "")
+//       .replace(/E/g, "")
+//       .replace(/O/g, "")
+//       .replace(/O'/g, "")
+//   );
+// }
+// ism("!@#Ragina");
+
+
+
+
+// function repeatStr (n, s) {
+//     return s.repeat(n)
+// }
+// console.log(repeatStr(3, 'hello'));
+
+
+// let text = 'hello'
+// let result = text.repeat(3)
+// console.log(result);
+
+
+
+// function remove(s) {
+//     return 
+// }
+// remove('#@&hello*#')
+
+
+
